cd489d056bd771437a88d4c9020852dcc1044376
isa-bilalli <isabilalli39@hotmail.com>
2025-12-26 09:58:34 +0100

    Tailwind Installation
    Tailwind v4 installed & working with react

--- frontend/loginow/src/App.jsx
+++ frontend/loginow/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="App">
+        <h1>Loginow</h1>
+        <p className='font-bold'>Welcome to Loginow! This is a placeholder for the login application.</p>
+      </div>
     </>
   )
 }
