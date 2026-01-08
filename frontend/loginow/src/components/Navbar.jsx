296f6d0e1c69b40cc4420605ff42983033de7536
isa-bilalli <isabilalli39@hotmail.com>
2026-01-08 17:54:44 +0100

    Navbar update
    Perditesove Navbar qe te kete reactive buttons ne baze se ku gjindet dhe bera Navbar responsive per screen

--- frontend/loginow/src/components/Navbar.jsx
+++ frontend/loginow/src/components/Navbar.jsx
@@ -1,27 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="bg-[#D9D9D9] w-50 h-screen flex flex-col items-center pt-4 px-4">
-      
-      {/* Logo */}
-      <img 
-        src="/src/assets/LogiNOW_WHITE-removebg-preview.png" 
-        className="w-36 sm:w-40 md:w-44 h-auto mb-10" 
-        alt="Logo"
-      />
-
-      {/* Navigation Links */}
+    <nav className="bg-[#D9D9D9] w-50 min-h-screen flex flex-col items-center pt-4 px-4">
+      <img src="/src/assets/LogiNOW_WHITE-removebg-preview.png" className="w-36 sm:w-40 md:w-44 h-auto mb-10" alt="Logo"/>
       <div className="flex flex-col w-full space-y-6">
-        <Link to="/dashboard" className="text-lg font-medium text-center py-2 px-4 rounded-lg bg-[#D9D9D9] hover:bg-[#B4B4B4] transition-colors text-white">Dashboard</Link>
-        <Link to="/postfreight" className="text-lg font-medium text-center py-2 px-4 rounded-lg bg-[#D9D9D9] hover:bg-[#B4B4B4] transition-colors text-white">Post Freight</Link>
-        <Link to="/findfreight" className="text-lg font-medium text-center py-2 px-4 rounded-lg bg-[#D9D9D9] hover:bg-[#B4B4B4] transition-colors text-white">Find Freight</Link>
-        <Link to="/posttruck" className="text-lg font-medium text-center py-2 px-4 rounded-lg bg-[#D9D9D9] hover:bg-[#B4B4B4] transition-colors text-white">Post Truck</Link>
-        <Link to="/findtruck" className="text-lg font-medium text-center py-2 px-4 rounded-lg bg-[#D9D9D9] hover:bg-[#B4B4B4] transition-colors text-white">Find Truck</Link>
-        <Link to="/myaccount" className="text-lg font-medium text-center py-2 px-4 rounded-lg bg-[#D9D9D9] hover:bg-[#B4B4B4] transition-colors text-white">My Account</Link>
+        <NavLink to="/dashboard" className={({ isActive }) => `text-lg font-medium text-center py-2 px-4 rounded-lg transition-colors duration-100 text-white ${isActive ? "bg-[#7ED957]":"hover:bg-[#B4B4B4]"}`}>Dashboard</NavLink>
+        <NavLink to="/postfreight" className={({ isActive }) => `text-lg font-medium text-center py-2 px-4 rounded-lg transition-colors duration-100 text-white ${isActive ? "bg-[#7ED957]":"hover:bg-[#B4B4B4]"}`}>Post Freight</NavLink>
+        <NavLink to="/findfreight" className={({ isActive }) => `text-lg font-medium text-center py-2 px-4 rounded-lg transition-colors duration-100 text-white ${isActive ? "bg-[#7ED957]":"hover:bg-[#B4B4B4]"}`}>Find Freight</NavLink>
+        <NavLink to="/posttruck" className={({ isActive }) => `text-lg font-medium text-center py-2 px-4 rounded-lg transition-colors duration-100 text-white ${isActive ? "bg-[#7ED957]":"hover:bg-[#B4B4B4]"}`}>Post Truck</NavLink>
+        <NavLink to="/findtruck" className={({ isActive }) => `text-lg font-medium text-center py-2 px-4 rounded-lg transition-colors duration-100 text-white ${isActive ? "bg-[#7ED957]":"hover:bg-[#B4B4B4]"}`}>Find Truck</NavLink>
+        <NavLink to="/myaccount" className={({ isActive }) => `text-lg font-medium text-center py-2 px-4 rounded-lg transition-colors duration-100 text-white ${isActive ? "bg-[#7ED957]":"hover:bg-[#B4B4B4]"}`}>My Account</NavLink>
       </div>
       <div className="mt-auto mb-6 w-full flex justify-center">
-        <Link to="/" className="flex items-center space-x-2 hover:opacity-80"><img src="/src/assets/logout-Photoroom.png" className="w-20 h-20 object-contain" alt="Logout Icon"/></Link>
+        <Link to="/" className="flex items-center space-x-2 hover:opacity-80"><img src="/src/assets/logout-Photoroom.png" className="w-16 h-16 object-contain" alt="Logout Icon"/></Link>
       </div>
     </nav>
   );
